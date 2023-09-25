6884cae46eea52edc55e519d5841a596d4ec01e0
vingitrepo <133174715+vingitrepo@users.noreply.github.com>
2023-09-25 18:48:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,8 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains: ")
 streamlit.dataframe(my_data_rows)
+
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+#This will not work correctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
