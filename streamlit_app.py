ac6c2b34d2d9d53fcfe72e9a3c033997f135ce1c
vingitrepo <133174715+vingitrepo@users.noreply.github.com>
2023-09-24 20:52:57 -0500

    Update streamlit_app.py
    Convert Fruityvice json response to table

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,11 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityservice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output in the screen as a table
+streamlit.dataframe(fruityvice_normalized)
