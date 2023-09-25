38b8d3f662adb0ba9de83c2ec62c79fcf69d3a92
vingitrepo <133174715+vingitrepo@users.noreply.github.com>
2023-09-25 16:14:56 -0500

    Update streamlit_app.py
    Added try except conditions

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,24 +24,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-
 #New section to display fruityservice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.write('The user entered ', fruit_choice)
+  else:
+    fruityvoice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-
-
-# take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output in the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
-
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -49,8 +46,6 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("")
 streamlit.text(my_data_row)
-
-
 
 streamlit.write('Thanks for adding ', add_my_fruit)
 
